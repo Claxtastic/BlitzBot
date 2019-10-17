469d0a0499c5b0c2b5d0d26ccef1e3a71331fde1
claxtont <claxtons4@gmail.com>
2019-10-16 22:53:07 -0500

    Fixed bug: following a query play with a url play no longer breaks the queue

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -68,40 +68,38 @@ export default class play implements IBotCommand {
             } catch (exception) {
                 console.log(exception);
             }
-
-        // else query is a search term
-        } else {
-    
-            try { 
-                // get one video (top result) from the search query
-                const videoResult: any[] = await this.youtube.searchVideos(query, 1);
-
-                // TODO?: Make 2nd parm not soft coded, add if (videoResult > 1) 
-
-                // get video ID of top result of query
-                const video = await this.youtube.getVideoByID(videoResult[0].id);
-
-                const url: string = video.url; 
-                const title: string = video.title;
-                const track = {
-                    url,
-                    title,
-                    voiceChannel
-                };
-
-                queue.push(track);
-                try {
-                    if (this._isPlaying == false) {
-                        msgObject.channel.send(`Playing ${track.title}`);
-                        return this.playTrack(queue);
-                    } else {
-                        // TODO: Add link
-                        return msgObject.channel.send(`${track.title} added to queue`);
-                    }
-                } catch (exception) { msgObject.channel.send(`Error playing track from bot: ${exception}`); }
-
-            } catch (exception) { console.log(exception); msgObject.channel.send(`Received error from YouTube: ${exception}`); }
         }
+    
+        try { 
+            // get one video (top result) from the search query
+            const videoResult: any[] = await this.youtube.searchVideos(query, 1);
+
+            // TODO?: Make 2nd parm not soft coded, add if (videoResult > 1) 
+
+            // get video ID of top result of query
+            const video = await this.youtube.getVideoByID(videoResult[0].id);
+
+            const url: string = video.url; 
+            const title: string = video.title;
+            const track = {
+                url,
+                title,
+                voiceChannel
+            };
+
+            queue.push(track);
+            try {
+                if (this._isPlaying == false) {
+                    this._isPlaying = true;
+                    msgObject.channel.send(`Playing ${track.title}`);
+                    return this.playTrack(queue);
+                } else if (this._isPlaying == true) {
+                    // TODO: Add link
+                    return msgObject.channel.send(`${track.title} added to queue`);
+                }
+            } catch (exception) { msgObject.channel.send(`Error playing track from bot: ${exception}`); }
+
+        } catch (exception) { console.log(exception); msgObject.channel.send(`Received error from YouTube: ${exception}`); }
     }
 
     playTrack(queue: Array<any>) {
